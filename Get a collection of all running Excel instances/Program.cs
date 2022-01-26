40d8e5069ad4473c75e1376a89bd164cba04254c
jakub <jakub@DESKTOP-DDN9ADE>
2022-01-26 12:24:36 +0100

    Add project files.

---
+++ Get a collection of all running Excel instances/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Microsoft.Office.Interop.Excel;
+
+
+namespace Get_a_collection_of_all_running_Excel_instances
+{
+
+    internal class Program
+    {
+        static void Main()
+        {
+
+            Application excel;
+
+            ExcelAppCollection myApps = new ExcelAppCollection();
+            Console.WriteLine("Session ID " + myApps.SessionID);
+            excel = myApps.PrimaryInstance;
+            //var oExcels = new List<Process>();
+
+            List<Process> oExcels = (List<Process>)myApps.GetProcesses();
+
+            Console.WriteLine("Number of Excel processes found: " + oExcels.Count);
+            Console.WriteLine();
+
+            foreach (Process process in oExcels)
+            {
+                excel = myApps.FromProcess(process);
+                Console.WriteLine("Process ID " + process.Id);
+                Console.WriteLine("Excel Workbooks count " + excel.Workbooks.Count);
+                Console.WriteLine();
+
+                foreach (Workbook workbook in excel.Workbooks)
+                {
+                    Console.WriteLine("Process ID: " + process.Id + " Workbook Name: " + workbook.Name);
+                }
+
+                Console.WriteLine();
+
+            }
+
+            Console.ReadLine(); 
+
+        }
+    }
+}
